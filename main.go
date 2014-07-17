abb04f8e594269415daa14f34f49fceb387525cb
unknown <sijieli@20140303-3663.KUGOU.GZ>
2014-07-17 11:32:24 +0800

    修正路由规则在新版beego失效的问题

--- main.go
+++ main.go
@@ -14,14 +14,14 @@ func main() {
 
 	//前台路由
 	beego.Router("/", &blog.MainController{}, "*:Index")
-	beego.Router("/:urlname([^/]+)", &blog.MainController{}, "*:Show") //别名访问
 	beego.Router("/page/:page:int", &blog.MainController{}, "*:Index")
 	beego.Router("/article/:id:int", &blog.MainController{}, "*:Show")         //ID访问
-	beego.Router("/article/:urlname([^/]+)", &blog.MainController{}, "*:Show") //别名访问
+	beego.Router("/article/:urlname(.+)", &blog.MainController{}, "*:Show") //别名访问
 	beego.Router("/archives", &blog.MainController{}, "*:Archives")
 	beego.Router("/archives/page/:page:int", &blog.MainController{}, "*:Archives")
-	beego.Router("/category/:name([^/]+)", &blog.MainController{}, "*:Category")
-	beego.Router("/category/:name([^/]+)/page/:page:int", &blog.MainController{}, "*:Category")
+	beego.Router("/category/:name(.+?)", &blog.MainController{}, "*:Category")
+	beego.Router("/category/:name(.+?)/page/:page:int", &blog.MainController{}, "*:Category")
+	beego.Router("/:urlname(.+)", &blog.MainController{}, "*:Show") //别名访问
 
 	//后台路由
 	beego.Router("/admin", &admin.IndexController{}, "*:Index")
